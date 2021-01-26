b918f75f94c18a4b2a2c3803a791a941d4b050fa
saam <samuel.aguila_cuba@outlook.com>
2021-01-26 12:09:28 -0500

    Setup Navigation-Component

---
+++ app/src/main/java/com/saam/myto_doapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.saam.myto_doapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.saam.myto_doapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/saam/myto_doapp/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.saam.myto_doapp.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.saam.myto_doapp.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+}
